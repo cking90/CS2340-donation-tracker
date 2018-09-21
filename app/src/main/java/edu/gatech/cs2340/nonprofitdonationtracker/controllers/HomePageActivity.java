7aa4acf86c81d0f59a541eb254b991d9c4dc486c
Charles King <cking90@gatech.edu>
2018-09-20 21:44:30 -0400

    Add login and logout functionality The login functionality is currently checked against a hard-coded Username: "user" and Password: "pass"

---
+++ app/src/main/java/edu/gatech/cs2340/nonprofitdonationtracker/controllers/HomePageActivity.java
@@ -0,0 +1,30 @@
+package edu.gatech.cs2340.nonprofitdonationtracker.controllers;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import edu.gatech.cs2340.nonprofitdonationtracker.R;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        Bundle extras = getIntent().getExtras();
+        String user_email = extras.getString("user_email");
+        TextView welcomeText = this.findViewById(R.id.welcomeTxtView);
+        String welcomeMsg = String.format("Welcome %s!", user_email.split("@")[0]);
+        welcomeText.setText(welcomeMsg);
+
+    }
+
+    public void onClickLogout(View view) {
+        Intent intent = new Intent(this, OpeningScreen.class);
+        startActivity(intent);
+    }
+}
