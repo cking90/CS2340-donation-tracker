b652f9a880a739ad06b29b438af60ac81ceed2d7
nehadesh <neha.n.deshpande@gmail.com>
2018-09-29 22:57:04 -0400

    Finished Registration Activity Added UserInfo class (temporary) to store user info Added UserType enum to store types of users TODO: Validate Login info with UserInfo Class

---
+++ app/src/main/java/edu/gatech/cs2340/nonprofitdonationtracker/controllers/UserInfo.java
@@ -0,0 +1,38 @@
+package edu.gatech.cs2340.nonprofitdonationtracker.controllers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Neha on 9/29/2018.
+ * This class is TEMPORARY
+ * It stores User info in a HashMap where the key = email id
+ * and the values = List containing name, password & user account type
+ */
+
+public class UserInfo {
+    private static Map<String, List<String>> loginInfo = new HashMap<>();
+
+    public boolean isValidLogin(String email, String password) {
+        if (!loginInfo.containsKey(email))
+            return false;
+        else {
+            if (!password.equals(loginInfo.get(email).get(1))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void addNewUser(String name, String email, String password, String userType) {
+        ArrayList<String> info = new ArrayList<>();
+        info.add(name);
+        info.add(password);
+        info.add(userType);
+        loginInfo.put(email, info);
+    }
+
+
+}
