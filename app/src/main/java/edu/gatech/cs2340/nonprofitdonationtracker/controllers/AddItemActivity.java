208b146625c8b361d7d16c532f77e22ab6533d06
prem99 <sakala.prem@gmail.com>
2018-10-20 20:31:05 -0400

    progress on add item page

--- app/src/main/java/edu/gatech/cs2340/nonprofitdonationtracker/controllers/AddItemActivity.java
+++ app/src/main/java/edu/gatech/cs2340/nonprofitdonationtracker/controllers/AddItemActivity.java
@@ -11,27 +11,38 @@ import edu.gatech.cs2340.nonprofitdonationtracker.R;
 
 public class AddItemActivity extends AppCompatActivity {
 
-    private EditText nameField;
+    private String nameField;
+    private String shortDescription;
+    private String longDescription;
+    private String price;
+    private Category category;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
 
-        nameField = (EditText) findViewById(R.id.itemName);
+        nameField = ((EditText) findViewById(R.id.itemName)).toString();
+        shortDescription = ((EditText) findViewById(R.id.shortDescription)).toString();
+        longDescription = ((EditText) findViewById(R.id.longDescription)).toString();
+        price = ((EditText) findViewById(R.id.price)).toString();
+
     }
 
     public void onCancelPressed(View view) {
-
+        //probably don't need this button because user can hit back button on phone
     }
 
+    //Prem - unsure on how to add the new donation into the correct location list
+    //feel like this is an easy fix for you charlie so I'm trying to finish the rest
+    //the category will be a spinner like the one in registration
     public void onAddItemPressed(View view) {
         Bundle extras = getIntent().getExtras();
         int id = extras.getInt("location_id");
         for (Location location: Location.locationList) {
             if (location.getKey() == id) {
-                location.addDonation(new Donation(nameField.toString(), null
-                        , null, 0, null));
+                location.addDonation(new Donation(nameField, shortDescription,
+                        longDescription, Float.parseFloat(price), category));
             }
             for(Donation donation: location.getDonations()) {
                 Log.d("donation", "" + donation);
