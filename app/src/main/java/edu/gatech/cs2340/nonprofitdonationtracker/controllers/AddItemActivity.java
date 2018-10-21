75d410c821a0d4521a85c02bf1653cf1b3797667
prem99 <sakala.prem@gmail.com>
2018-10-21 16:51:34 -0400

    progress on add item page

--- app/src/main/java/edu/gatech/cs2340/nonprofitdonationtracker/controllers/AddItemActivity.java
+++ app/src/main/java/edu/gatech/cs2340/nonprofitdonationtracker/controllers/AddItemActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.util.Log;
+import android.widget.Spinner;
 
 import edu.gatech.cs2340.nonprofitdonationtracker.R;
 
@@ -15,7 +17,7 @@ public class AddItemActivity extends AppCompatActivity {
     private String shortDescription;
     private String longDescription;
     private String price;
-    private Category category;
+    private Spinner categorySpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,11 @@ public class AddItemActivity extends AppCompatActivity {
         shortDescription = ((EditText) findViewById(R.id.shortDescription)).toString();
         longDescription = ((EditText) findViewById(R.id.longDescription)).toString();
         price = ((EditText) findViewById(R.id.price)).toString();
+        categorySpinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, Category.values());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(adapter);
 
     }
 
@@ -35,14 +42,14 @@ public class AddItemActivity extends AppCompatActivity {
 
     //Prem - unsure on how to add the new donation into the correct location list
     //feel like this is an easy fix for you charlie so I'm trying to finish the rest
-    //the category will be a spinner like the one in registration
+    //also probably need to add implementation to prevent duplicate items
     public void onAddItemPressed(View view) {
         Bundle extras = getIntent().getExtras();
         int id = extras.getInt("location_id");
         for (Location location: Location.locationList) {
             if (location.getKey() == id) {
                 location.addDonation(new Donation(nameField, shortDescription,
-                        longDescription, Float.parseFloat(price), category));
+                        longDescription, Float.parseFloat(price), (Category) categorySpinner.getSelectedItem()));
             }
             for(Donation donation: location.getDonations()) {
                 Log.d("donation", "" + donation);
