13fe0a54e92558cd24f1641e4379ffbba61c6259
cking90 <cking90@gatech.edu>
2018-10-28 17:20:16 -0400

    Add search activity controller

---
+++ app/src/main/java/edu/gatech/cs2340/nonprofitdonationtracker/controllers/SearchActivity.java
@@ -0,0 +1,15 @@
+package edu.gatech.cs2340.nonprofitdonationtracker.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.cs2340.nonprofitdonationtracker.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
