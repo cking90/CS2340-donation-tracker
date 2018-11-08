4807ee810c5f0ff7913155eea34e8abb4c14dc46
cking90 <cking90@gatech.edu>
2018-11-08 18:42:18 -0500

    Separate Location and LocationType classes

--- app/src/test/java/edu/gatech/cs2340/nonprofitdonationtracker/LocationEqualsTest.java
+++ app/src/test/java/edu/gatech/cs2340/nonprofitdonationtracker/LocationEqualsTest.java
@@ -6,10 +6,10 @@ import edu.gatech.cs2340.nonprofitdonationtracker.controllers.Location;
 import edu.gatech.cs2340.nonprofitdonationtracker.controllers.LocationType;
 
 public class LocationEqualsTest {
-    @Test
-    public void testEquals() {
-        Location location1 = new Location(3, "Dummy Location", 12,
-                                30, "5252 Baker Street", "Evans", "GA",
-                                30809, LocationType.DROPOFF,
-    }
+//    @Test
+//    public void testEquals() {
+//        Location location1 = new Location(3, "Dummy Location", 12,
+//                                30, "5252 Baker Street", "Evans", "GA",
+//                                30809, LocationType.DROPOFF,
+//    }
 }
