38fcec4744bb0ea20ade7447b45e872fd2493d80
Tyler <tsmith394@gatech.edu>
2018-11-15 18:42:27 -0500

    fixed some spelling errors

--- app/src/test/java/edu/gatech/cs2340/nonprofitdonationtracker/CorrectPasswordTest.java
+++ app/src/test/java/edu/gatech/cs2340/nonprofitdonationtracker/CorrectPasswordTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import edu.gatech.cs2340.nonprofitdonationtracker.models.UserInfo;
 
+import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 
 /**
@@ -19,6 +20,12 @@ public class CorrectPasswordTest {
     public void correctPassword() {
         UserInfo user = new UserInfo();
         UserInfo.addUserToLocal("name", "email", "password", "Admin");
+
+        //email is not valid
+        assertFalse(UserInfo.correctPassword("f", null));
+
+        //email valid, password not
+        assertFalse(UserInfo.correctPassword("email", "incorrect"));
 
         //email and password valid
         assertTrue(UserInfo.correctPassword("email", "password"));
